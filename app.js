bbc2903ebbb39cfa917c413379e672f0ae987c33
berkanankal <berkan.ankal@outlook.com>
2022-07-09 14:19:36 +0300

    header & slider completed

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // Batuhan
 
-
-
 // Berkan
 
-
+$(".header-carousel").owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  items: 1,
+  autoplay: true,
+});
 
 // Evren
 
-
-
 // Ruken
-
-
 
 // Ufuk
